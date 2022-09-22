1fffaaec8cd941d83475c3fe0325848e277281d7
dryego barbosa <dryego.lisboa@gmail.com>
2022-09-22 00:00:13 -0300

    Resolução, carros.

---
+++ 02/index.js
@@ -0,0 +1,29 @@
+const carro = {
+    marca: 'Fiat',
+    modelo: 'UNO',
+    ano: 1996,
+    cor: 'vermelho',
+    quantidade_portas: 4,
+    automatico: false
+}
+const carro1 = {
+    marca: 'Honda',
+    modelo: 'New Civic',
+    ano: 2011,
+    cor: 'prata',
+    quantidade_portas: 4,
+    automatico: true
+
+}
+const carro2 = {
+    marca: 'Chevrolet',
+    modelo: 'S-10',
+    ano: 2009,
+    cor: 'Preto',
+    quantidade_portas: 4,
+    automatico: false
+}
+console.log(carro);
+console.log(carro1);
+console.log(carro2);
+
