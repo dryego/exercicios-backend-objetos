d7ee7092e4739235cfc46d68b33776ccd03de8fc
dryego barbosa <dryego.lisboa@gmail.com>
2022-09-22 00:17:59 -0300

    Resolução, desestruturação.

---
+++ 03/index.js
@@ -0,0 +1,9 @@
+const usuario = {
+    nome: "Dryego",
+    idade: 33,
+    profissao: "programador",
+    cor_preferida: "vermelho"
+}
+const { nome, idade, profissao, cor_preferida } = usuario
+
+console.log(`Ola, eu sou ${nome}, tenho ${idade} anos de idade. Sou ${profissao} e minha cor preferida é ${cor_preferida}.`);
