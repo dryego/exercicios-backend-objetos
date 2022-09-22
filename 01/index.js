95d0102311092ed052b003392737f12c4da44b70
dryego barbosa <dryego.lisboa@gmail.com>
2022-09-21 23:23:02 -0300

    Resolução, endereço usuario.

---
+++ 01/index.js
@@ -0,0 +1,19 @@
+const endereco = {
+    rua: 'Av. 01',
+    numero: 45,
+    complemento: 'A',
+    bairro: 'São Paulo',
+    CEP: '54756-040',
+    cidade: 'Camaragibe',
+    estado: 'PE',
+    pais: 'Brasil'
+}
+const usuario = {
+    nome: 'Dryego',
+    email: 'dryego.lisboa@gmail.com',
+    telefone: '8198202.7154',
+    dataNascimento: '28/03/1988',
+    endereco
+}
+
+console.log(usuario);
